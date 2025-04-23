dd4df919c9817256a09b2e00d56c563bf318d78e
Gopinath WorkSpace <gbnathworkspace@gmail.com>
2025-04-23 20:21:49 +0530

    - updated security filters for API endpoints

--- src/main/java/com/expense/expensetracker/repository/UserRepository.java
+++ src/main/java/com/expense/expensetracker/repository/UserRepository.java
@@ -2,11 +2,15 @@ package com.expense.expensetracker.repository;
 
 import com.expense.expensetracker.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
     Optional<User> findByEmail(String email);
+    Optional<User> findByUsername(String name);
 
 }
+
